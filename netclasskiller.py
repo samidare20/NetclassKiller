9d396b4956037f2fca033650d2daccc4ef869528
Your Name <you@example.com>
2019-09-30 16:55:36 +0900

    1

--- netclasskiller.py
+++ netclasskiller.py
@@ -1,5 +1,30 @@
 import subprocess
-from PyQt5 import *
+import sys
+from PyQt5.QtWidgets import *
+'''
 target=['SvcNC80Cli.exe','NC80Cli.exe','rncHost.exe','NC80Cap.exe','SvcNC80Cli.exe']
 for i in target:
     subprocess.call('taskkill /F /IM '+i)
+    '''
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.setWindowTitle('My First Application')
+        self.move(300, 300)
+        self.resize(400, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
