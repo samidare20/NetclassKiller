de24108885099ebdc0b153242850a809e39482eb
Your Name <you@example.com>
2019-09-30 16:46:50 +0900

    remove #

--- netclasskiller.py
+++ netclasskiller.py
@@ -1,5 +1,5 @@
 import subprocess
+from PyQt5 import *
 target=['SvcNC80Cli.exe','NC80Cli.exe','rncHost.exe','NC80Cap.exe','SvcNC80Cli.exe']
 for i in target:
-    #print("code:"+'taskkill /F /IM '+target[i])
     subprocess.call('taskkill /F /IM '+i)
