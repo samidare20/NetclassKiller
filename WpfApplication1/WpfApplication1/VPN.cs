a52b474c3af93149ac02f5ad209a3db6af8150dc
Your Name <you@example.com>
2021-04-19 15:11:34 +0900

    changed vpn name

--- WpfApplication1/WpfApplication1/VPN.cs
+++ WpfApplication1/WpfApplication1/VPN.cs
@@ -7,10 +7,10 @@ class VPN
     public void run(command cmd,TextBlock processbox)
     {
         processbox.Text = "Trying connect VPN...";
-        cmd.go("powershell Add-VpnConnection -Name \"chika.kr\" -ServerAddress \"vpn1.shiki.kr\" -TunnelType L2tp -L2tpPsk railgun\n\ny\n");
+        cmd.go("powershell Add-VpnConnection -Name \"Evans_ChikaVPN\" -ServerAddress \"vpn1.chika.kr\" -TunnelType L2tp -L2tpPsk railgun\n\ny\n");
         Thread.Sleep(3000);
         processbox.Text += "\nTrying login VPN...";
-        cmd.go("powershell rasdial chika.kr sunrin1 tsuki");
+        cmd.go("powershell rasdial Evans_ChikaVPN sunrin1 tsuki");
         processbox.Text += "\nConnecting VPN complete!";
         MessageBox.Show("Connecting VPN complete!");
     }
